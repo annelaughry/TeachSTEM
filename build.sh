c404001ec5fa1a9b69a24a28b66e35fe9ed8ac92
Anne White <annelaughry@gmail.com>
2026-07-15 12:11:52 -0400

    Commit React build to git so Render can serve it without Node.js

--- build.sh
+++ build.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
-set -e  # exit immediately if any command fails
+set -e
 
 echo "--- Installing Python dependencies ---"
 pip install -r requirements.txt
-
-echo "--- Building React frontend ---"
-cd frontend
-npm install
-npm run build
-cd ..
 
 echo "--- Collecting Django static files ---"
 python manage.py collectstatic --noinput
